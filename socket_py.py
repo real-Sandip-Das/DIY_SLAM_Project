2f0cb7b46f0576d2cdafcd8c3d3b9d18063c2a1d
real-Sandip-Das <sd13535@outlook.com>
2024-03-26 23:08:37 +0530

    Updated the program for Python UDP Program

--- socket_py.py
+++ socket_py.py
@@ -1,7 +1,8 @@
 import socket
 
-UDP_IP = "0.0.0.0"  # Listen on all available interfaces
-UDP_PORT = 8888
+UDP_IP = "127.0.0.1"  # Listen on all available interfaces
+UDP_PORT = 4210
+MESSAGE = "Hello from Python to NodeMCU" #Message to be sent from Laptop to NodeMCU
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
@@ -11,4 +12,11 @@ print("UDP server listening...")
 while True:
     data, addr = sock.recvfrom(1024)  # Buffer size is 1024 bytes
     print("Received message:", data.decode())
+
+    print("UDP target IP:", UDP_IP)
+    print("UDP target port:", UDP_PORT)
+    print("message:", MESSAGE)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+    sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
 
