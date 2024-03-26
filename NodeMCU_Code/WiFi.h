5a605588ef837c8ead53b07e1d2585fe4adc367b
soumil-kumar <hellosoumil@gmail.com>
2024-03-27 00:16:50 +0530

    wifi.h

--- NodeMCU_Code/WiFi.h
+++ NodeMCU_Code/WiFi.h
@@ -4,5 +4,12 @@
 #define WIFI_PASSWORD "5<069fE2"
 */
 //Soumil's Hotspot
-#define WIFI_SSID "SOUMIL-LAPTOP 3013"
+
+#define WIFI_SSID "SOUMIL"
 #define WIFI_PASSWORD "12345678"
+
+//Sandip's Hotspot
+/*
+#define WIFI_SSID "Xiaomi"
+#define WIFI_PASSWORD "esp8266_"
+*/
