50b9c0cf2c3d8ac285b1fd52472f97dcd0ee4850
soumil-kumar <hellosoumil@gmail.com>
2024-03-26 23:24:40 +0530

    UDP

--- NodeMCU_Code/WiFi.h
+++ NodeMCU_Code/WiFi.h
@@ -1,3 +1,8 @@
 //Aditya's Hotspot
+/*
 #define WIFI_SSID "ADITYAACER5 7430"
 #define WIFI_PASSWORD "5<069fE2"
+*/
+//Soumil's Hotspot
+#define WIFI_SSID "SOUMIL-LAPTOP 3013"
+#define WIFI_PASSWORD "12345678"
--- socket_py.py
+++ socket_py.py
@@ -1,11 +1,12 @@
 import socket
 
-UDP_IP = "127.0.0.1"  # Listen on all available interfaces
+SELF_IP = "0.0.0.0"  # Listen on all available interfaces
 UDP_PORT = 4210
+ESP_IP = "192.168.137.164"
 MESSAGE = "Hello from Python to NodeMCU" #Message to be sent from Laptop to NodeMCU
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
+sock.bind((SELF_IP, UDP_PORT))
 
 print("UDP server listening...")
 
@@ -13,7 +14,7 @@ while True:
     data, addr = sock.recvfrom(1024)  # Buffer size is 1024 bytes
     print("Received message:", data.decode())
 
-    print("UDP target IP:", UDP_IP)
+    print("UDP target IP:", ESP_IP)
     print("UDP target port:", UDP_PORT)
     print("message:", MESSAGE)
 
