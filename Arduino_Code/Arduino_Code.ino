315ae3921ab95c067fecffa315afa5e2f05f5438
Soumil Kumar <143015922+soumil-kumar@users.noreply.github.com>
2024-03-30 22:27:47 +0530

    Update Arduino_Code.ino

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 #include "Controls.h"
 #include "servo.h"
-
+int angle;
 #define SERVO_PIN 13
 
 Servo myServo;
@@ -11,22 +11,23 @@ char given_state = 'J';
 
 void setup() {
   Serial.begin(9600);  // Set the baud rate to match the communication with the ESP826
-  motor_setup(m1p1, m1p2, m2p1, m2p2, m1speed, m2speed);
+  motor_setup();
   myServo.write(90);
   myServo.attach(SERVO_PIN);
+  angle = 90;
 }
 
 void loop() {
-  int angle = 90;
+
   if (Serial.available() > 0) {
     char state = Serial.read();
     if (state == 'J' || state == 'W' || state == 'A' || state == 'S' || state == 'D') {
       motor_loop(state);
-    } else if (state == 'p') {
-      angle++;
+    } else if (state == 'P') {
+      angle +=10;
       myServo.write(angle);
-    } else if (state == 'n') {
-      angle--;
+    } else if (state == 'N') {
+      angle -=10;
       myServo.write(angle);
     }
   }
