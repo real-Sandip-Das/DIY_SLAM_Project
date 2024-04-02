2174ac0b5a748948e75c305a83cc96cd03a90200
AdityaSinghBisht100 <165181264+AdityaSinghBisht100@users.noreply.github.com>
2024-04-02 14:38:03 +0530

    Hotspot Trial

--- NodeMCU_Code/NodeMCU_Code.ino
+++ NodeMCU_Code/NodeMCU_Code.ino
@@ -1,40 +1,20 @@
 #include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
 
-#include "WiFi.h"
-
-#define UDP_PORT 4210
-
-WiFiUDP UDP;
-char packet[256];
+const char *ssid = "NodeMCU_AP";      // SSID of the NodeMCU hotspot
+const char *password = "password123"; // Password for the NodeMCU hotspot
 
 void setup() {
-  scan_setup(); //sets up the Ultrasonic Sensor for Measuring Distances
-  Serial.begin(9600);
-  //try connecting to WiFi Hotspot
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.begin(115200);
+  
+  // Set the NodeMCU as an access point
+  WiFi.softAP(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(100);
-  }
-  //connected to WiFi
-  UDP.begin(UDP_PORT);
+  IPAddress ip = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(ip);
+  Serial.print("Local IP: ", WiFi.localIP());
 }
 
 void loop() {
-  int packetSize = UDP.parsePacket(); //Parse Packet received over UDP
-  if (packetSize) {
-    int len = UDP.read(packet, 255); //Read Packet received over UDP
-    if (len > 0) {
-      packet[len] = '\0';
-    }
-
-    if (packet == "Q") {
-      //If the Packet reads 'Q' then scan using Servo Motor and Ultrasonic sensor
-      scan(UDP);
-    } else {
-      //If the Packet reads 'W', 'A', 'S' or 'D', then move accordingly, else, if 'J' then wait
-      Serial.print(packet);
-    }
-  }
+  // Code here
 }
